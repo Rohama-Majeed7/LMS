d6c41d0de442b98616be57533b0ed7cc721d6d2e
Rohama Majeed <rohamamajeed3@gmail.com>
2026-05-19 14:06:03 +0300

    files clear

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-[80vh] my-4 bg-gray-100">
-      <SignIn  />
-    </div>
-  );
-}
